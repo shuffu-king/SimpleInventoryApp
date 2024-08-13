19229b8d0a46d095d6db8c5d5edd885963fa6c34
shuffu-king <shafau_ayo@outlook.com>
2024-08-14 00:11:08 +0100

    Added site swapping robots functionality

---
+++ FireInventory/Core/Sites/Robots/SiteRobotSwap.swift
@@ -0,0 +1,18 @@
+//
+//  SiteRobotSwap.swift
+//  FireInventory
+//
+//  Created by Ayo Shafau on 8/13/24.
+//
+
+import SwiftUI
+
+struct SiteRobotSwap: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SiteRobotSwap()
+}
