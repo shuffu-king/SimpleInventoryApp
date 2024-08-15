32fc0e6d78faa4e39580b4a979ca9b757e18ecea
shuffu-king <shafau_ayo@outlook.com>
2024-08-15 19:43:18 +0100

    Added damaaged items list and functions.

---
+++ FireInventory/Core/Sites/Items/DeletedItemUpdateView.swift
@@ -0,0 +1,18 @@
+//
+//  DeletedItemUpdateView.swift
+//  FireInventory
+//
+//  Created by Ayo Shafau on 8/15/24.
+//
+
+import SwiftUI
+
+struct DeletedItemUpdateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DeletedItemUpdateView()
+}
---
+++ FireInventory/Core/Sites/Items/ItemTransferView.swift
@@ -0,0 +1,18 @@
+//
+//  ItemTransferView.swift
+//  FireInventory
+//
+//  Created by Ayo Shafau on 8/14/24.
+//
+
+import SwiftUI
+
+struct ItemTransferView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItemTransferView()
+}
